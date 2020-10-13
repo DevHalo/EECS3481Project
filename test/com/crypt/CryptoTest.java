34cd63f52cfb91faf7b468ccf247f9d42094dc00
Mark <voongm@my.yorku.ca>
2020-10-13 02:53:43 -0400

    Updated Test Class

--- test/com/crypt/CryptoTest.java
+++ test/com/crypt/CryptoTest.java
@@ -1,35 +1,78 @@
 package com.crypt;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 import java.nio.file.Files;
-import java.nio.file.Paths;
+
 import java.security.MessageDigest;
 
 import com.crypt.algorithms.Utilities;
 import com.crypt.algorithms.XOR;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.io.*;
 import java.security.NoSuchAlgorithmException;
+import java.util.Random;
 
 public class CryptoTest {
 
-    File[] testFiles;
-    String[] md5s;
+    private File[] testFiles;
+    private String[] md5s;
+
+    private final String WORKING_DIRECTORY = System.getProperty("user.dir") + "/out/test/EECS3481Project/";
+    private String key;
+
+    /**
+     * Pretty text for organizing the output of different encryption algorithms.
+     * Shows before the method is run.
+     */
+    @BeforeEach
+    void PrintBefore(TestInfo testInfo) {
+        System.out.printf("| Commencing %s |%n", testInfo.getDisplayName());
+    }
+
+    /**
+     * Pretty text for organizing the output of different encryption algorithms.
+     * Shows after the method is run
+     */
+    @AfterEach
+    void PrintAfter(TestInfo testInfo) {
+        System.out.printf("| End of %s |%n", testInfo.getDisplayName());
+    }
 
     /**
      * Converts MD5 checksum to hexadecimal string
      */
-    String ByteToHexString(byte[] bytes)
-    {
+    String ByteToHexString(byte[] bytes) {
         StringBuilder sb = new StringBuilder();
 
         for (byte b : bytes)
             sb.append(String.format("%02x", b));
 
         return sb.toString();
+    }
+
+    /**
+     * Generates a pseudorandom alphanumeric key of length bytes
+     *
+     * @param bytes Length of the key in number of bytes
+     */
+    void GenerateKey(int bytes) {
+        String alphanumeric = "ABCDEFGHIJKLMNOPQRSTUVXYZ0123456789";
+        StringBuilder sb = new StringBuilder();
+        Random rand = new Random();
+
+        for (int i = 0; i < bytes; i++) {
+            sb.append(alphanumeric.charAt((int) (rand.nextFloat() * alphanumeric.length())));
+        }
+        key = sb.toString();
+
+        System.out.printf("Generated key of length %d bytes:%n%s%n", bytes, key);
     }
 
     /**
@@ -46,53 +89,68 @@ public class CryptoTest {
         System.out.println("Moving test files to working directory...");
 
         for (int i = 0; i < testFiles.length; i++) {
-            System.out.printf("Moving file: %s to: %s \\ %s%n", testFiles[i].getName(),
-                    System.getProperty("user.dir"), testFiles[i].getName());
+            System.out.printf("Moving file: %s to: %s%s%n", testFiles[i].getName(),
+                    WORKING_DIRECTORY, testFiles[i].getName());
 
             // Copy files from test folder to working directory, then calculate MD5 from original file.
             try {
                 byte[] in = Files.readAllBytes(testFiles[i].toPath());
-                FileOutputStream out = new FileOutputStream(new File(System.getProperty("user.dir") + "/" +
-                        testFiles[i].getName()));
-                MessageDigest md = MessageDigest.getInstance("MD5");
-                md.reset();
+
+                FileOutputStream out = new FileOutputStream(new File(WORKING_DIRECTORY +
+                        testFiles[i].getName()), false);
 
                 out.write(in);
                 out.close();
 
-                md5s[i] = ByteToHexString(md.digest(in));
-                System.out.printf("FILE NAME: %s | MD5 CHECKSUM:%n%s%n", testFiles[i].getName(), md5s[i]);
+                MessageDigest md = MessageDigest.getInstance("MD5");
+                md.reset();
 
-            } catch (FileNotFoundException | NoSuchAlgorithmException e) {
-                fail("This shouldn't happen.");
+                md5s[i] = ByteToHexString(md.digest(in));
+
+                System.out.printf("FILE NAME: %s | MD5 CHECKSUM:%n%s%n", testFiles[i].getName(), md5s[i]);
+            } catch (FileNotFoundException fileNotFoundException) {
+                fail("Failed to find input file: " + testFiles[i].getName());
+            } catch (NoSuchAlgorithmException noSuchAlgorithmException) {
+                fail("Failed to initialize MD5 Message Digest");
             } catch (IOException ioException) {
                 fail("An error occurred attempting to copy the test file to the working directory.");
             }
         }
+
+        // Generate a pseudorandom alphanumeric key used to encrypt/decrypt
+        GenerateKey(8);
+        System.out.println();
     }
 
     /**
      * Tests the XOR encryption and decryption algorithm
      */
     @Test
+    @DisplayName("XOR Encryption and Decryption Test")
     void XORTest() {
-        System.out.println("Commencing XOR Encrypt/Decrypt Test");
-
         for (int i = 0; i < testFiles.length; i++) {
+            File fileName = new File(WORKING_DIRECTORY + testFiles[i].getName());
+
+            // Encrypt file here
             try {
-                byte[] input = Files.readAllBytes(Paths.get(System.getProperty("user.dir") + "/" +
-                        testFiles[i].getName()));
-                // encrypt file here
-                File fileName = new File (System.getProperty("user.dir") +"/"+ testFiles[i].getName());
-                XOR.xorFile(fileName.toString(), "MMAAD".getBytes(), true);
+                XOR.xorFile(fileName.toString(), key.getBytes(), true);
+            } catch (Exception e) {
+                e.printStackTrace();
+                fail("Failed to encrypt file " + i + "/" + testFiles.length + ": " + fileName.getName() + "%n");
+            }
 
-                //fileName = System.getProperty("user.dir") +"/"+ testFiles[i].getName();
-                // decrypt file here
-                XOR.xorFile(fileName.toString() + Utilities.ENCRYPTED_EXTENSION, "MMAAD".getBytes(), false);
+            // Decrypt file here
+            try {
+                XOR.xorFile(fileName.toString() + Utilities.ENCRYPTED_EXTENSION,
+                        key.getBytes(), false);
+            } catch (Exception e) {
+                e.printStackTrace();
+                fail("Failed to decrypt file " + i + "/" + testFiles.length + ": " + fileName.getName() + "%n");
+            }
 
+            try {
+                byte[] output = Files.readAllBytes(fileName.toPath());
 
-                byte[] output = Files.readAllBytes(Paths.get(System.getProperty("user.dir") + "/" +
-                        testFiles[i].getName())); // TEST REMOVE LATER
                 // Check MD5 checksum for match
                 MessageDigest md = MessageDigest.getInstance("MD5");
                 md.reset();
@@ -101,9 +159,9 @@ public class CryptoTest {
 
                 assertEquals(md5s[i], ByteToHexString(alteredmd5));
             } catch (IOException ioException) {
-                fail("Failed to read decrypted file.");
+                fail("Failed to read decrypted file " + fileName.toString());
             } catch (NoSuchAlgorithmException noAlgoException) {
-                fail("This shouldn't happen.");
+                fail("Could not initialize MD5 Message Digest");
             }
         }
     }
