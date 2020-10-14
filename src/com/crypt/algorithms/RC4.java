85f7e713638cef671590aa0198f39a58ebc8e0dc
Mark <voongm@my.yorku.ca>
2020-10-14 06:03:37 -0400

    RC4 Implemented

--- src/com/crypt/algorithms/RC4.java
+++ src/com/crypt/algorithms/RC4.java
@@ -1,5 +1,30 @@
 package com.crypt.algorithms;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
 // Implementation of the RC4 algorithm
 public class RC4 {
+
+    public static void crypt(String fileName, byte[] startingKey, boolean encrypt) {
+        try {
+            Cipher cipher = Cipher.getInstance("ARCFOUR");
+            SecretKeySpec key = new SecretKeySpec(startingKey, cipher.getAlgorithm());
+
+            cipher.init(encrypt ? Cipher.ENCRYPT_MODE : Cipher.DECRYPT_MODE, key);
+
+            byte[] input = Utilities.readFile(fileName);
+            byte[] output = cipher.doFinal(input);
+
+            Utilities.writeFile(output, fileName, encrypt);
+        } catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException |
+                IllegalBlockSizeException | BadPaddingException e) {
+            e.printStackTrace();
+        }
+    }
 }
