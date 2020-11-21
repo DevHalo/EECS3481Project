cd0dfd93728db4a55d792140bbfc0750c868697d
Mark <voongm@my.yorku.ca>
2020-11-21 05:16:52 -0500

    ECC and RSA Implementation

--- src/com/crypt/algorithms/ECC.java
+++ src/com/crypt/algorithms/ECC.java
@@ -1,5 +1,49 @@
 package com.crypt.algorithms;
 
+import javax.crypto.KeyAgreement;
+import java.security.*;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
+
 // Implementation of the ECC algorithm
 public class ECC {
+    public static void crypt(String filePath, byte[] privateKey, byte[] publicKey, boolean encrypt) {
+        try {
+            // Convert byte arrays to private and public key objects
+            KeyFactory kf = KeyFactory.getInstance("EC");
+            PrivateKey prKey = kf.generatePrivate(new PKCS8EncodedKeySpec(Base64.getDecoder().decode(privateKey)));
+            PublicKey puKey = kf.generatePublic(new X509EncodedKeySpec(Base64.getDecoder().decode(publicKey)));
+
+            // Do the key agreement with third party public key and user's private key via Diffie-Hellman
+            KeyAgreement keyAgreement = KeyAgreement.getInstance("ECDH");
+            keyAgreement.init(prKey);
+            keyAgreement.doPhase(puKey, true);
+
+            // Hash the key to fit the correct size
+            MessageDigest md = MessageDigest.getInstance("SHA256");
+
+            // Encrypt the file using the hashed shared secret as our AES key
+            AES.crypt(filePath, md.digest(keyAgreement.generateSecret()), encrypt);
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException | InvalidKeyException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Generates an ECC public and private key pair.
+     * @return A private and public KeyPair
+     */
+    public static KeyPair generateECCPair() {
+        try {
+            KeyPairGenerator kg = KeyPairGenerator.getInstance("EC");
+            kg.initialize(256);
+
+            return kg.generateKeyPair();
+        } catch (NoSuchAlgorithmException noSuchAlgorithmException) {
+            noSuchAlgorithmException.printStackTrace();
+        }
+        return null;
+    }
 }
--- src/com/crypt/algorithms/RSA.java
+++ src/com/crypt/algorithms/RSA.java
@@ -1,12 +1,10 @@
 package com.crypt.algorithms;
 
-import javax.crypto.BadPaddingException;
-import javax.crypto.Cipher;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.NoSuchPaddingException;
-import java.io.File;
+import javax.crypto.*;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.security.*;
+import java.security.interfaces.RSAPrivateKey;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
@@ -14,94 +12,70 @@ import java.util.Base64;
 
 // Implementation of the RSA algorithm
 public class RSA {
-
-    private static String publicKey;
-    private static String privateKey;
-
-    public static void createKeys() throws NoSuchAlgorithmException, IOException {
-        Utilities.RSAKeyPairGenerator();
-        publicKey = Base64.getEncoder().encodeToString(Utilities.getPublicKey().getEncoded());
-        privateKey = Base64.getEncoder().encodeToString(Utilities.getPrivateKey().getEncoded());
-    }
-
-    public static byte[] readKeysFromFile(String fileName)
-    {
-       return Utilities.readFile(fileName);
-
-    }
-
-    public static PublicKey getPublicKey(String base64PublicKey){
-        PublicKey publicKey = null;
-        try{
-            X509EncodedKeySpec keySpec = new X509EncodedKeySpec(Base64.getDecoder().decode(base64PublicKey.getBytes()));
-            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
-            publicKey = keyFactory.generatePublic(keySpec);
-            return publicKey;
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        } catch (InvalidKeySpecException e) {
-            e.printStackTrace();
-        }
-        return publicKey;
-    }
-
-    public static PrivateKey getPrivateKey(String base64PrivateKey){
-        PrivateKey privateKey = null;
-        PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(Base64.getDecoder().decode(base64PrivateKey.getBytes()));
-        KeyFactory keyFactory = null;
+    public static void crypt(String fileName, byte[] key, boolean encrypt) {
         try {
-            keyFactory = KeyFactory.getInstance("RSA");
-        } catch (NoSuchAlgorithmException e) {
+            Cipher cipher = Cipher.getInstance("RSA/ECB/OAEPWithSHA-256AndMGF1Padding");
+            KeyFactory kf = KeyFactory.getInstance("RSA");
+
+            if (encrypt) {
+                // Convert byte array to public key object
+                X509EncodedKeySpec publicKeySpec = new X509EncodedKeySpec(Base64.getDecoder().decode(key));
+
+                cipher.init(Cipher.ENCRYPT_MODE, kf.generatePublic(publicKeySpec));
+
+                KeyGenerator kg = KeyGenerator.getInstance("AES");
+                kg.init(256);
+
+                SecretKey AESkey = kg.generateKey();
+
+                AES.crypt(fileName, AESkey.getEncoded(), true);
+
+                // Encrypt AES key and append it to the file
+                byte[] encryptedAES = cipher.doFinal(AESkey.getEncoded());
+
+                FileOutputStream fs = new FileOutputStream(fileName + Utilities.ENCRYPTED_EXTENSION, true);
+                fs.write(encryptedAES);
+                fs.close();
+            } else {
+                // Convert byte array to private key object
+                PKCS8EncodedKeySpec privateKeySpec = new PKCS8EncodedKeySpec(Base64.getDecoder().decode(key));
+                PrivateKey pk = kf.generatePrivate(privateKeySpec);
+
+                cipher.init(Cipher.DECRYPT_MODE, pk);
+
+                // Grab AES key that was appended at the end of the file
+                byte[] encryptedAES = Utilities.readDataAtOffset(fileName,
+                        ((RSAPrivateKey) pk).getModulus().bitLength() / 8, 0, true);
+
+                // Decrypt AES key
+                byte[] decryptedAES = cipher.doFinal(encryptedAES);
+
+                // Truncate the AES key from the file
+                Utilities.truncateDataAtEOF(encryptedAES, fileName);
+
+                // Decrypt the file
+                AES.crypt(fileName, decryptedAES, false);
+            }
+        } catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeySpecException |
+                InvalidKeyException | BadPaddingException | IllegalBlockSizeException | IOException e) {
             e.printStackTrace();
         }
+    }
+
+    /**
+     * Generates a RSA key pair
+     * @param bits number of bits of the modulus portion
+     * @return
+     */
+    public static KeyPair generateRSAPair(int bits) {
         try {
-            privateKey = keyFactory.generatePrivate(keySpec);
-        } catch (InvalidKeySpecException e) {
-            e.printStackTrace();
+            KeyPairGenerator kg = KeyPairGenerator.getInstance("RSA");
+            kg.initialize(bits);
+            return kg.generateKeyPair();
+        } catch (NoSuchAlgorithmException noSuchAlgorithmException) {
+            noSuchAlgorithmException.printStackTrace();
         }
-        return privateKey;
+
+        return null;
     }
-
-    public static byte[] encrypt(byte[] data, String publicKey) throws BadPaddingException, IllegalBlockSizeException, InvalidKeyException, NoSuchPaddingException, NoSuchAlgorithmException {
-        Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
-        cipher.init(Cipher.ENCRYPT_MODE, getPublicKey(publicKey));
-        return cipher.doFinal(data);
-    }
-
-    public static byte[] decrypt(byte[] data, String privateKey) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
-        Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
-        cipher.init(Cipher.DECRYPT_MODE, getPrivateKey(privateKey));
-        return cipher.doFinal(data);
-    }
-
-    public static void crypt(String fileName, byte[] startingKey, boolean encrypt) {
-        try {
-            // Read input bytes from file and initialize output array
-            byte[] input = Utilities.readFile(fileName);
-            byte[] output = new byte[input.length];
-            if (new File("publicKey.txt").exists())
-            {
-                publicKey = new String(readKeysFromFile("publicKey.txt"));
-                privateKey = new String(readKeysFromFile("privateKey.txt"));
-            }
-            else
-            {
-                createKeys();
-            }
-
-            if(encrypt)
-            {
-                output = encrypt(input , publicKey);
-            }
-            else
-            {
-                output = decrypt(input , privateKey);
-            }
-            // Write output to file
-            Utilities.writeFile(output, fileName, encrypt);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
 }
